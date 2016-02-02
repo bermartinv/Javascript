6c4ef1e2b6953e23cb20d087bcbd1b2513919e7a
Bernardo Martin <bermartinv@gmail.com>
2016-02-02 23:45:58 +0100

    ejercicio 30

--- ejercicios/ejercicio39/funciones.js
+++ ejercicios/ejercicio39/funciones.js
@@ -1,13 +1,13 @@
 var editando = false;
 
-function editar(nodo){
+function editar(nodo) {
 var padre=nodo.parentNode;
 var nombre_padre=padre.nodeName;
 
 
- //document.getElementById("sacar").innerHTML=nombre_padre;   
+ 
  var hijo=padre.getElementsByTagName("td");
-    //document.getElementById("sacar").innerHTML=hijo.length; 
+    
  
  if (editando == false) {
 
@@ -24,19 +24,42 @@ var nuevoHTML='<td><input type="text" name="alimento" id="alimento" value="'+ali
 '<td><input type="text" name="grasas" id="grasas" value="'+grasas+'" size="5"</td>'+
 '<td><input type="text" name="proteina" id="proteina" value="'+proteina+'" size="5"</td>'+
 '<td><input type="text" name="carbohidratos" id="carbohidratos" value="'+carbohidratos+'" size="5"</td> <td>En edición</td>';
-padre.innerHTML=nuevoHTML;
-     
+padre.innerHTML=nuevoHTML;    
  nodoContenedorForm.innerHTML = 'Pulse Aceptar para guardar los cambios o cancelar para anularlos'+
+'<form name = "formulario" action="http://aprenderaprogramar.com" method="get" onsubmit="capturarEnvio()" onreset="anular()">'+
+'<input class="boton" type = "submit" value="Aceptar"> <input class="boton" type="reset" value="Cancelar">';
+editando = "true";}
+else {alert ('Solo se puede editar una línea. Recargue la página para poder editar otra');
+}    
+}
+
+ function capturarEnvio(){
+
+var nodoContenedorForm = document.getElementById('contenedorForm'); //Nodo DIV
+
+nodoContenedorForm.innerHTML = 'Pulse Aceptar para guardar los cambios o cancelar para anularlos'+
 
 '<form name = "formulario" action="http://aprenderaprogramar.com" method="get" onsubmit="capturarEnvio()" onreset="anular()">'+
 
+'<input type="hidden" name="alimento" value="'+document.querySelector('#alimento').value+'">'+
+
+'<input type="hidden" name="calorias" value="'+document.querySelector('#calorias').value+'">'+
+
+'<input type="hidden" name="grasas" value="'+document.querySelector('#grasas').value+'">'+
+
+'<input type="hidden" name="proteina" value="'+document.querySelector('#proteina').value+'">'+
+
+'<input type="hidden" name="carbohidratos" value="'+document.querySelector('#carbohidratos').value+'">'+
+
 '<input class="boton" type = "submit" value="Aceptar"> <input class="boton" type="reset" value="Cancelar">';
 
-editando = "true";}
-
-else {alert ('Solo se puede editar una línea. Recargue la página para poder editar otra');
-
+document.formulario.submit();
 
 }
-    
+
+ 
+
+function anular(){
+
+window.location.reload();
 }
